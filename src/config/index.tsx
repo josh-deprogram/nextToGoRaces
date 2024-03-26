1dc9766306765b8d08273c72f1f6f3746ce2bf32
Josh Freeman <info@deprogram.io>
2024-03-26 22:39:43 +1100

    Scaffold project directories

---
+++ src/config/index.tsx
@@ -0,0 +1,28 @@
+export const size = {
+  small: 8,
+  medium: 16,
+  large: 32,
+  xl: 48,
+};
+
+export const font = {
+  small: 8,
+  body: 16,
+  title: 24,
+  xl: 38,
+};
+
+export const colors = {
+  light: '#fff',
+  primary: '#ff0000',
+  secondary: '#000',
+  dark: 'rgba(0,0,0,0.8)',
+  medium: 'rgba(0,0,0,0.4)',
+  border: 'rgba(0,0,0,0.1)',
+};
+
+export default {
+  size,
+  font,
+  colors,
+};
